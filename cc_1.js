12aa0da56b3f396e2b87a5b7bd6d22c9cdb4f5f6
rajeeve12 <rajeevevishen21@gmal.com>
2024-09-01 17:21:25 -0400

    Task 2 product inventory management

--- cc_1.js
+++ cc_1.js
@@ -2,6 +2,14 @@
 let employeename = 'Bob'
 const employeeID = 1
 var isActive = true
-console.log(employeename)
-console.log(employeeID)
-console.log(isActive)
+console.log(employeename);
+console.log(employeeID);
+console.log(isActive);
+
+
+let products = new Array();
+let productDetails = {
+    name: 'product', price = 2, inStock = true
+};
+ console.log(products);
+ console.log(productDetails);
