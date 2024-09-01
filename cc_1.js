541292c2ae28bcf2f1e949bf08372ffd77a4fc61
rajeeve12 <rajeevevishen21@gmal.com>
2024-09-01 17:34:08 -0400

    Task 3 Financial Calculations

--- cc_1.js
+++ cc_1.js
@@ -13,3 +13,16 @@ let productDetails = {
 };
  console.log(products);
  console.log(productDetails);
+
+ let accountBalance = 0;
+ let updatedBalance = accountBalance +=1;
+ console.log(updatedBalance);
+ let updatedBalance2 = accountBalance -=2;
+ console.log(updatedBalance2);
+
+ let updatedBalance3 = accountBalance *=2;
+ console.log(updatedBalance3);
+
+ let updatedBalance4 = accountBalance /=3;
+ console.log(updatedBalance4);
+
